3c11de6d5d3fb70bfd3ecd1351d669997ffd58dc
kthss01 <kthss01@naver.com>
2024-07-20 23:37:56 +0900

    캐릭터 스프라이트시트 설정

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 Phaser 개발을 위해 Phaser 공부 및 정리를 위한 공간
 
 ## 캐릭터
+
+### spritesheet
+
+frameWidth, frameHeight 설정 방법은
+
+이미지보고 계산하는 방법도 있고
+
+그냥 debug 키고 대충 맞춰보는 것도 방법인거 같음
+
+## Debug
+
+physics가 있어야 그때부터 debug 모드가 먹힘
--- character_move/src/main.js
+++ character_move/src/main.js
@@ -11,36 +11,48 @@ class MyGame extends Phaser.Scene {
 		this.load.image("background1", bgImg1);
 
 		this.load.spritesheet("player", playerImg, {
-			frameWidth: 32,
-			frameHeight: 36,
-		});
+			frameWidth: 48,
+			frameHeight: 48,
+		}); // debug 키고 사이즈 대충 확인해서 조절함 192 x 192 4 x 4 라 48이 맞음
 	}
 
 	create() {
-		this.background1 = this.add.image(0, 0, "background1");
-		this.background1.setOrigin(0, 0);
+		// this.background1 = this.add.image(0, 0, "background1");
+		// this.background1.setOrigin(0, 0);
 
-		// sprite
-		this.player = this.add.sprite(
+		// image
+		this.player = this.add.image(
 			config.width / 2,
 			config.height / 2,
-			"player"
+			"player",
+			0 // 0, 4, 8, 12
 		);
+
+		// sprite
+		// this.player = this.add.sprite(
+		// 	config.width / 2,
+		// 	config.height / 2,
+		// 	"player"
+		// );
 
 		this.player.scale = 3;
 
-		this.anims.create({
-			key: "player_idle",
-			frames: this.anims.generateFrameNumbers("player", {
-				start: 0,
-				end: 0,
-			}),
-			frameRate: 1,
-			repeat: 0, // 무제한
-		});
+		// this.anims.create({
+		// 	key: "player_idle",
+		// 	frames: this.anims.generateFrameNumbers("player", {
+		// 		start: 0,
+		// 		end: 0,
+		// 	}),
+		// 	frameRate: 1,
+		// 	repeat: 0, // 무제한
+		// });
 
-		this.player.play("player_idle");
+		// this.player.play("player_idle");
+
+		this.physics.add.existing(this.player, true);
 	}
+
+	update(time, delta) {}
 }
 
 const config = {
@@ -53,6 +65,7 @@ const config = {
 		default: "arcade",
 		arcade: {
 			debug: true,
+			gravity: { y: 200 },
 		},
 	},
 	scene: MyGame,
