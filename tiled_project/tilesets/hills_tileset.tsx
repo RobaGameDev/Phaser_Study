c64a6906134c0450c90398e6ebd4474e3b1197ee
kthss01 <kthss01@naver.com>
2024-07-21 11:52:26 +0900

    tilemap collide property 처리, collision graphic debug 처리

--- tiled_project/tilesets/hills_tileset.tsx
+++ tiled_project/tilesets/hills_tileset.tsx
@@ -3,42 +3,42 @@
  <image source="../assets/Hills.png" width="176" height="112"/>
  <tile id="0">
   <properties>
-   <property name="colliders" type="bool" value="true"/>
+   <property name="collides" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="1">
   <properties>
-   <property name="colliders" type="bool" value="true"/>
+   <property name="collides" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="2">
   <properties>
-   <property name="colliders" type="bool" value="true"/>
+   <property name="collides" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="11">
   <properties>
-   <property name="colliders" type="bool" value="true"/>
+   <property name="collides" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="13">
   <properties>
-   <property name="colliders" type="bool" value="true"/>
+   <property name="collides" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="22">
   <properties>
-   <property name="colliders" type="bool" value="true"/>
+   <property name="collides" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="23">
   <properties>
-   <property name="colliders" type="bool" value="true"/>
+   <property name="collides" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="24">
   <properties>
-   <property name="colliders" type="bool" value="true"/>
+   <property name="collides" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="33">
--- tilemap/src/main.js
+++ tilemap/src/main.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import tiles from "./assets/Hills.png";
+import balls from "./assets/balls.png";
 
 class MyGame extends Phaser.Scene {
 	constructor() {
@@ -9,19 +10,44 @@ class MyGame extends Phaser.Scene {
 	preload() {
 		this.load.image("tiles", tiles);
 		this.load.tilemapTiledJSON("map", "/assets/sample_map.tmj");
+
+		this.load.spritesheet("balls", balls, {
+			frameWidth: 17,
+			frameHeight: 17,
+		});
 	}
 
 	create() {
-		const map = this.make.tilemap({ key: "map" });
+		this.map = this.make.tilemap({ key: "map" });
 
-		const tiles = map.addTilesetImage("hills_tileset", "tiles");
+		const tiles = this.map.addTilesetImage("hills_tileset", "tiles");
 
-		const layer = map.createLayer(0, tiles, 0, 0);
+		const layer = this.map.createLayer(0, tiles, 0, 0);
 
 		layer.scale = 2;
+
+		// 충돌 처리
+		layer.setCollisionByProperty({ collides: true });
+
+		// 충동 draw 처리
+		// Visualize the colliding tiles
+		this.debugGraphics = this.add.graphics();
+		this.drawDebug();
+
+		this.input.on("pointerdown", () => {
+			this.debugGraphics.visible = !this.debugGraphics.visible;
+			if (this.debugGraphics.visible) {
+				this.drawDebug();
+			}
+		});
 	}
 
 	update(time, delta) {}
+
+	drawDebug() {
+		this.debugGraphics.clear();
+		this.map.renderDebug(this.debugGraphics, { tileColor: null });
+	}
 }
 
 const config = {
