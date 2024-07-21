01b514d69030acad068f0f942403c6bbd7c5660f
kthss01 <kthss01@naver.com>
2024-07-21 10:48:22 +0900

    tilemap json export

--- tiled_project/tilesets/hills_tileset.tsx
+++ tiled_project/tilesets/hills_tileset.tsx
@@ -1,4 +1,59 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="hills_tileset" tilewidth="16" tileheight="16" tilecount="77" columns="11">
  <image source="../assets/Hills.png" width="176" height="112"/>
+ <tile id="0">
+  <properties>
+   <property name="colliders" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="colliders" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="colliders" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="colliders" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="colliders" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="colliders" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="colliders" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="colliders" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="colliders" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="colliders" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="colliders" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
