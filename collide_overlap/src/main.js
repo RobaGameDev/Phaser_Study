f18109d7fa4066fba1fda5a88a14639d7382648f
kthss01 <kthss01@naver.com>
2024-07-21 13:46:35 +0900

    collide overlap 처리

--- collide_overlap/src/main.js
+++ collide_overlap/src/main.js
@@ -1,15 +1,79 @@
 import Phaser from "phaser";
+import balls from "./assets/balls.png";
 
 class MyGame extends Phaser.Scene {
 	constructor() {
 		super();
 	}
 
-	preload() {}
+	preload() {
+		// spritesheet load
+		this.load.spritesheet("balls", balls, {
+			frameWidth: 17,
+			frameHeight: 17,
+		});
+	}
 
-	create() {}
+	create() {
+		// ball
+		this.ball = this.physics.add.image(
+			config.width / 2,
+			config.height / 2,
+			"balls",
+			0
+		);
+		// ball2
+		this.ball2 = this.physics.add.image(
+			config.width / 2,
+			config.height / 4,
+			"balls",
+			1
+		);
+		// ball3
+		this.ball3 = this.physics.add.image(
+			config.width / 4,
+			config.height / 2,
+			"balls",
+			2
+		);
 
-	update(time, delta) {}
+		// keyboard input
+		this.cursors = this.input.keyboard.createCursorKeys();
+
+		// physics
+		this.physics.add.existing(this.ball);
+		this.physics.add.existing(this.ball2);
+		this.physics.add.existing(this.ball3);
+	}
+
+	update(time, delta) {
+		this.move();
+
+		// collide
+		this.physics.collide(this.ball, this.ball2);
+
+		// overlap
+		this.physics.overlap(this.ball, this.ball3, (ball, ball3) => {
+			ball3.destroy();
+		});
+	}
+
+	move() {
+		this.ball.setVelocity(0);
+		const SPEED = 150;
+
+		if (this.cursors.left.isDown) {
+			this.ball.setVelocityX(-SPEED);
+		} else if (this.cursors.right.isDown) {
+			this.ball.setVelocityX(SPEED);
+		}
+
+		if (this.cursors.up.isDown) {
+			this.ball.setVelocityY(-SPEED);
+		} else if (this.cursors.down.isDown) {
+			this.ball.setVelocityY(SPEED);
+		}
+	}
 }
 
 const config = {
